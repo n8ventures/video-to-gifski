5a3a435c71e2a1b121c2d036d9bed0def500744b
n8ventures <n8ventures@gmail.com>
2024-02-12 04:07:51 +0800

    added versions (manually)

--- __version__.py
+++ __version__.py
@@ -3,3 +3,8 @@ __appname__ = "n8-vid-to-gif"
 
 __updaterversion__ = "1.0.0"
 __updatername__="n8gif-updater"
+
+__ffmpegfullversion__= '2024-02-04-git-7375a6ca7b-full_build-www.gyan.dev'
+__ffmpegliteversion__= '2024-02-04-git-7375a6ca7b-essentials_build-www.gyan.dev'
+__ffmpegversion__='2024-02-04-git-7375a6ca7b'
+__gifskiversion__='1.14.4'
