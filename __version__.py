59495a377775ef2e8b09358ce921c6df47dbcddf
n8ventures <n8ventures@gmail.com>
2024-02-13 05:41:24 +0800

    2.1.0d

--- __version__.py
+++ __version__.py
@@ -1,8 +1,8 @@
-__version__ = "2.1.0cock-ball-torture" #2.1.0
+__version__ = "2.1.0d"
 __appname__ = "n8-vid-to-gif"
 
 __updaterversion__ = "1.0.0"
 __updatername__="n8gif-updater"
 
-__ffmpegversion__='2024-02-04-git-7375a6ca7b'
-__gifskiversion__='1.14.4'
+__ffmpegversion__ = '2024-02-04-git-7375a6ca7b'
+__gifskiversion__= '1.14.4'
