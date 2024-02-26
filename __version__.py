046768e925d348d5c3766324a98d99210c12cc45
n8ventures <n8ventures@gmail.com>
2024-02-27 02:31:03 +0800

    2.3.0 - Updated nightly build of FFMPEG

--- __version__.py
+++ __version__.py
@@ -4,5 +4,5 @@ __appname__ = "n8-vid-to-gif"
 __updaterversion__ = "1.1.0"
 __updatername__="n8-gif-updater"
 
-__ffmpegversion__ = '2024-02-22-git-76b2bb96b4'
+__ffmpegversion__ = '2024-02-26-git-a3ca4beeaa'
 __gifskiversion__= '1.14.4'
