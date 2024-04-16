83b314e1a9d1691655df25238f8815a315387ea7
John Nathaniel Calvara <nate@n8ventures.dev>
2024-04-16 11:28:02 +0800

    Mac Version

--- __version__.py
+++ __version__.py
@@ -1,4 +1,5 @@
 __version__ = "2.4.0"
+__versionMac__ = "2.4.1-beta"
 __appname__ = "n8-vid-to-gif"
 
 __updaterversion__ = "1.1.1"
@@ -6,3 +7,6 @@ __updatername__="n8-gif-updater"
 
 __ffmpegversion__ = '2024-03-18-git-a32f75d6e2'
 __gifskiversion__= '1.14.4'
+
+__ffmpegversion_Mac__ = 'ffmpeg-114775-g359b6a7f8a'
+__gifskiversion_Mac__= '1.14.4'
