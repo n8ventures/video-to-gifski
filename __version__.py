66cade15b53e7bb10ceac74b7bdfd7b5eee4ec3a
n8ventures <nate@n8ventures.dev>
2024-11-30 14:32:20 +0800

    updated win ffmpeg

--- __version__.py
+++ __version__.py
@@ -6,7 +6,7 @@ __author__= 'John Nathaniel Calvara'
 __updaterversion__ = "1.1.1"
 __updatername__="n8-gif-updater"
 
-__ffmpegversion__ = '2024-06-21-git-d45e20c37b'
-__gifskiversion__= '1.14.4'
+__ffmpegversion__ = '2024-11-28-git-bc991ca048'
+__gifskiversion__= '1.32.0'
 __ffmpegversion_Mac__ = 'N-117868-gecc7d5db9c'
 __gifskiversion_Mac__= '1.14.4'
