87a738d850d342be2dab3e82a18eb1caa0392012
n8ventures <nate@n8ventures.dev>
2024-11-30 12:47:28 +0800

    2.6.0 FFMPEG 7 (64ARM) OSX

--- __version__.py
+++ __version__.py
@@ -1,5 +1,5 @@
-__version__ = "2.5.2"
-__versionMac__ = "2.5.2"
+__version__ = "2.6.0"
+__versionMac__ = "2.6.0"
 __appname__ = "n8-vid-to-gif"
 __author__= 'John Nathaniel Calvara'
 
@@ -8,5 +8,5 @@ __updatername__="n8-gif-updater"
 
 __ffmpegversion__ = '2024-06-21-git-d45e20c37b'
 __gifskiversion__= '1.14.4'
-__ffmpegversion_Mac__ = 'ffmpeg-114775-g359b6a7f8a'
+__ffmpegversion_Mac__ = 'N-117868-gecc7d5db9c'
 __gifskiversion_Mac__= '1.14.4'
