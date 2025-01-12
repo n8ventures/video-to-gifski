5f20440751e04d279e6c5881a6f6040472ea9b4c
n8ventures <nate@n8ventures.dev>
2025-01-12 11:37:37 +0800

    updated ffmpeg for apple silicon

--- __version__.py
+++ __version__.py
@@ -5,5 +5,5 @@ __author__= 'John Nathaniel Calvara'
 
 __ffmpegversion__ = '2025-01-08-git-251de1791e'
 __gifskiversion__= '1.32.0'
-__ffmpegversion_Mac__ = 'N-117868-gecc7d5db9c'
+__ffmpegversion_Mac__ = 'N-118282-gf651ca3a68'
 __gifskiversion_Mac__= '1.14.4'
