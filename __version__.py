5c16e33abc0b22cb95ff0c0c075a2b12db9a12e2
n8ventures <nate@n8ventures.dev>
2025-01-12 08:46:56 +0800

    4.0.0

--- __version__.py
+++ __version__.py
@@ -1,12 +1,9 @@
-__version__ = "4.0.0-alpha"
-__versionMac__ = "4.0.0-alpha"
-__appname__ = "n8-vid-to-gif"
+__version__ = "4.0.0"
+__versionMac__ = "4.0.0"
+__appname__ = "N8\'s Video To Gifski"
 __author__= 'John Nathaniel Calvara'
 
-__updaterversion__ = "1.1.1"
-__updatername__="n8-gif-updater"
-
-__ffmpegversion__ = '2024-11-28-git-bc991ca048'
+__ffmpegversion__ = '2025-01-08-git-251de1791e'
 __gifskiversion__= '1.32.0'
 __ffmpegversion_Mac__ = 'N-117868-gecc7d5db9c'
 __gifskiversion_Mac__= '1.14.4'
