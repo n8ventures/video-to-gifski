2219f579844c1ae2900c94d9f5f8ab9db28baec2
John Nathaniel Calvara <nate@n8ventures.dev>
2025-08-12 14:39:23 +0800

    4.0.2

--- __version__.py
+++ __version__.py
@@ -1,9 +1,9 @@
 __version__ = "4.0.2"
-__versionMac__ = "4.0.1"
+__versionMac__ = "4.0.2"
 __appname__ = "N8\'s Video To Gifski"
 __author__= 'John Nathaniel Calvara'
 
 __ffmpegversion__ = '2025-08-04-git-9a32b86307'
 __gifskiversion__= '1.34.0'
-__ffmpegversion_Mac__ = 'N-118282-gf651ca3a68'
-__gifskiversion_Mac__= '1.14.4'
+__ffmpegversion_Mac__ = 'N-120653-g85e8e59001'
+__gifskiversion_Mac__= '1.34.0'
